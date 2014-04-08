1286048133f371411d73591b710f437bc45ceeb0
Thomas Rega <thomas.rega@momox.biz>
2014-04-08 07:52:27 +0200

    Example for local file
    python manage.py runserver --settings=quizshowdown.staging

---
+++ django/quizshowdown/staging.py
@@ -0,0 +1,14 @@
+from settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '%#c098o(yg1am!fg_9fxu$00kr$@@fn918x*=gq8tpzo6bxbx5'
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-handlebars', '{} {{infile}}'.format('django-ember-precompile')),
+    ('text/coffeescript', '/Users/thoreg/node_modules/coffee-script/bin/coffee --compile --stdio'),
+)
