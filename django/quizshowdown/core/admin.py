104466c64684b6f7f485953ca8f574e507fa0e8d
Thomas Rega <thomas.rega@momox.biz>
2014-04-18 16:44:32 +0200

    Allow access via admin interface to get data in quickly

--- django/quizshowdown/core/admin.py
+++ django/quizshowdown/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from quizshowdown.core.models import Answer, Category, Quiz, UserProfile
 
-# Register your models here.
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    fk_name = "quiz"
+
+
+class QuizAdmin(admin.ModelAdmin):
+    inlines = [
+        AnswerInline,
+    ]
+
+
+admin.site.register(Category)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(UserProfile)
